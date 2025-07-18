80a7c7a4bbf746152d62becd5f1488995dd5841f
Martin Dablander <martin@Martins-MacBook-Air.local>
2025-07-18 12:00:50 +0900

    changed repeated categories to be created by loop

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,42 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const categories = [
+    {
+      id: 1,
+      title: "Hats"
+    },
+    {
+      id: 2,
+      title: "Jackets"
+    },
+    {
+      id: 3,
+      title: "Sneakers"
+    },
+    {
+      id: 4,
+      title: "Womens"
+    },
+    {
+      id: 5,
+      title: "Mens"
+    },
+  ]
 
-  return (
+  return (categories.map(({ title }) => (
     <div className='categories-container'>
       <div className='category-container'>
-        {/* <img /> */}
+        <div className='background-image' />
         <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
     </div>
+  ))
+    
   )
 }
 
